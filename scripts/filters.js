f0e2db1362787778ac7f3706de69a9310d548e2d
Arold Mauger <aroldmauger2022@gmail.com>
2023-09-28 20:54:09 +0200

    Travail sur le CSS du filtre ingrédient

--- scripts/filters.js
+++ scripts/filters.js
@@ -1,17 +1,21 @@
 import { recipes } from "./recipes.js"
 
 // OUVERTURE - FERMETURE DU FILTRE INGREDIENTS//
-const searchIngredientFilter = document.querySelector(".search-ingredient-in-filter");
+const searchIngredientFilter = document.querySelector(".search-in-filter");
 const buttonIngredients = document.querySelector(".ingredient-button");
 const chevronDown = document.querySelector(".bi-chevron-down");
 
 buttonIngredients.addEventListener("click", function (){
     if(searchIngredientFilter.style.display === "none") {
         searchIngredientFilter.style.display = "block";
+        buttonIngredients.style.borderRadius  = "15px 15px 0px 0px";
+
         chevronDown.classList.add("rotate");   
     } else {
         searchIngredientFilter.style.display = "none";
         chevronDown.classList.remove("rotate");  
+        buttonIngredients.style.borderRadius  = "15px 15px 15px 15px";
+
     }
                     
 });
@@ -26,30 +30,26 @@ recipes.forEach((recipe) => {
 });
 
 const uniqueIngredientsArray = Array.from(listUniqueIngredients);
-const allIngredientsText = uniqueIngredientsArray.join('<br>');
+
 const allIngredientsList = document.querySelector(".import-ingredients");
-allIngredientsList.innerHTML = allIngredientsText;
 
+uniqueIngredientsArray.forEach((ingredient) => {
+  const ingredientElement = document.createElement('span');
+  ingredientElement.textContent = ingredient;
+  ingredientElement.classList.add('items-in-filter');
 
+  allIngredientsList.appendChild(ingredientElement);
 
+  ingredientElement.addEventListener('mouseover', () => {   // GESTION DU SURVOL ET FOND JAUNE
+    ingredientElement.style.backgroundColor = '#FFD15B';
+  });
 
-// OUVERTURE - FERMETURE DU FILTRE APPAREILS//
-const searchApplianceFilter = document.querySelector(".search-appliance-in-filter");
-const buttonAppliances = document.querySelector(".appliance-button");
-const chevronDownAppliance = document.querySelector(".appliance-chevron");
-
-buttonAppliances.addEventListener("click", function (){
-    if(searchApplianceFilter.style.display === "none") {
-        searchApplianceFilter.style.display = "block";
-        chevronDownAppliance.classList.add("rotate");   
-    } else {
-        searchApplianceFilter.style.display = "none";
-        chevronDownAppliance.classList.remove("rotate");  
-    }
-                    
+  ingredientElement.addEventListener('mouseout', () => {
+    ingredientElement.style.backgroundColor = 'transparent';
+  });
 });
 
-
+/*
 // AFFICHAGE DE TOUS LES APPAREILS A LA SUITE
 const listUniqueAppliances = new Set();
 
@@ -61,3 +61,4 @@ const uniqueAppliancesArray = Array.from(listUniqueAppliances);
 const allAppliancesText = uniqueAppliancesArray.join('<br>');
 const allAppliancesList = document.querySelector(".import-appliances");
 allAppliancesList.innerHTML = allAppliancesText;
+*/
