ad3f9e0893b783ac0ef340cd24f5d7bf33828699
Arold Mauger <aroldmauger2022@gmail.com>
2023-09-30 17:38:32 +0200

    Apparition de la croix dans la barre de recherche du filtre ingrédients réussie

--- scripts/filters.js
+++ scripts/filters.js
@@ -4,23 +4,29 @@ import { recipes } from "./recipes.js"
 const searchIngredientFilter = document.querySelector(".search-in-filter");
 const buttonIngredients = document.querySelector(".ingredient-button");
 const chevronDown = document.querySelector(".bi-chevron-down");
+const ingredientFilterInput = document.getElementById('search-ingredient');     // ici on vise l'input dans le HTML
+const closeButtonInSearch = document.querySelector(".close-in-search");
+const allIngredientsList = document.querySelector(".import-ingredients");
 
 buttonIngredients.addEventListener("click", function (){
     if(searchIngredientFilter.style.display === "none") {
         searchIngredientFilter.style.display = "block";
         buttonIngredients.style.borderRadius  = "15px 15px 0px 0px";
-
-        chevronDown.classList.add("rotate");   
+        chevronDown.classList.add("rotate");  
+        allIngredientsList.innerHTML = "";
+        displayAllIngredients()
     } else {
         searchIngredientFilter.style.display = "none";
         chevronDown.classList.remove("rotate");  
         buttonIngredients.style.borderRadius  = "15px 15px 15px 15px";
-
+        ingredientFilterInput.value = "";
+        closeButtonInSearch.style.display = "none";
     }
                     
 });
 
 // AFFICHAGE DE TOUS LES INGREDIENTS A LA SUITE
+function displayAllIngredients () {
 const listUniqueIngredients = new Set();
 
 recipes.forEach((recipe) => {
@@ -30,8 +36,6 @@ recipes.forEach((recipe) => {
 });
 
 const uniqueIngredientsArray = Array.from(listUniqueIngredients);
-
-const allIngredientsList = document.querySelector(".import-ingredients");
 
 uniqueIngredientsArray.forEach((ingredient) => {
   const ingredientElement = document.createElement('span');
@@ -61,6 +65,7 @@ uniqueIngredientsArray.forEach((ingredient) => {
     tagIngredient.textContent = ingredient;
     const closeTagIngredient = document.createElement("span");
     closeTagIngredient.classList.add("bi-x-lg");
+    closeTagIngredient.classList.add("close-tag-button");
     closeTagIngredient.addEventListener("click", function(){
     tagContainer.style.display ="none";
     })
@@ -70,24 +75,53 @@ uniqueIngredientsArray.forEach((ingredient) => {
     allTagsContainer.appendChild(tagContainer)
 });
 
+});
+filterIngredientsFromInput()
 
-const ingredientFilterInput = document.getElementById('search-ingredient');     // ici on vise l'input dans le HTML
-const ingredientItems = document.querySelectorAll('.items-in-filter');
+}
 
-ingredientFilterInput.addEventListener('input', (event) => {
-  const filterText = removeAccents(event.target.value.toLowerCase());           // ici, le texte qu'on écrit dans l'input sans accent
+// FONCTION POUR FILTRER LES NOMS DES INGREDIENTS EN FONCTION DE CE QU'ECRIT L'UTILISATEUR DANS LA BARRE DE RECHERCHE
+function filterIngredientsFromInput () {
+  const ingredientFilterInput = document.getElementById('search-ingredient');     // ici on vise l'input dans le HTML
+  const ingredientItems = document.querySelectorAll('.items-in-filter');
+
+  ingredientFilterInput.addEventListener('input', (event) => {
+    const filterText = removeAccents(event.target.value.toLowerCase());           // ici, le texte qu'on écrit dans l'input sans accent
                                                                                 // ToLowerCase() nous permet de comparer l'input et les ingrédients avec la même casse
-  ingredientItems.forEach((ingredientItem) => { 
-    const ingredientName = removeAccents(ingredientItem.textContent.toLowerCase()); // ici, l'ingrédient dans la liste sans accent
+    ingredientItems.forEach((ingredientItem) => { 
+      ingredientItem.addEventListener("click", function() {                   // permet de supprimer le texte dans la barre de recherche 
+        event.target.value = "";   
+        closeButtonInSearch.style.display = "none";
 
-    if (ingredientName.includes(filterText)) {
-      ingredientItem.style.display = 'block'; 
-    } else {
-      ingredientItem.style.display = 'none'; 
-    }
+      })
+      const ingredientName = removeAccents(ingredientItem.textContent.toLowerCase()); // ici, l'ingrédient dans la liste sans accent
+      if (ingredientName.includes(filterText)) {
+        ingredientItem.style.display = 'block'; 
+      } else {
+        ingredientItem.style.display = 'none'; 
+      }
+    });
   });
-});
-});
+
+}
+
+// FONCTION POUR FAIRE APPARAITRE LA CROIX DANS LA BARRE DE RECHERCHE
+function displayCloseButtonInSearchBar (event) {
+
+  if(event.target.value === "") {                                              
+    closeButtonInSearch.style.display = 'none';
+  } else {
+    closeButtonInSearch.style.display = 'block'; 
+  }
+  closeButtonInSearch.addEventListener("click", function (e){
+    ingredientFilterInput.value = "";
+    allIngredientsList.innerHTML = "";
+    displayAllIngredients()
+  })
+
+}
+
+ingredientFilterInput.addEventListener('input', displayCloseButtonInSearchBar);
 
 // FONCTION POUR NORMALISER LES CARACTERES RECHERCHÉS DANS L'INPUT SANS ACCENTS
 function removeAccents(str) {
